3ff12fcaf4792d756a92999e946979f59e54b012
Luis Angel Perez Melendez <angelmelen88@gmail.com>
2022-10-29 16:29:20 -0500

    modo oscuro

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 const colors = require('tailwindcss/colors')
 module.exports = {
   content: ["./public/index.html", "./src/**/*.{html,js}"],
-  darkMode: false,
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
     colors: {
       ...colors,
@@ -14,23 +14,6 @@ module.exports = {
       Montserrat: ["Montserrat", "sans-serif"],
     },
     extend: {
-      backgroundImage: {
-        'sanFrancisco': "url('./public/img/sanFrancisco.jpg')",
-        'sanFranciscoDesktop': "url('./public/img/sanFranciscoDesktop.jpg')",
-        'yosemite': "url('./public/img/yosemite.jpg')",
-        'LA': "url('./public/img/LA.jpg')",
-        'seattle': "url('./public/img/seattle.jpg')",
-        'new_york': "url('./public/img/new_york.jpg')",
-        'norway': "url('./public/img/norway.jpg')",
-        'sydney': "url('./public/img/sydney.jpg')",
-        'miami': "url('./public/img/miami.jpg')",
-        'switzerland': "url('./public/img/switzerland.jpg')", 
-        'bali': "url('./public/img/bali.jpg')",
-        'norway': "url('./public/img/norway.jpg')",
-        'chicago': "url('./public/img/chicago.jpg')",
-        'europe': "url('./public/img/europe.jpg')",
-        'iceland': "url('./public/img/iceland.jpg')",
-      },
     },
   },
   variants:{
